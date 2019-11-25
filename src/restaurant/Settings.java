e71c8a246a50a0090408446ee7c688c935fbd0b6
KamilBaranowski <tmp2206@gmail.com>
2019-11-25 20:24:17 +0100

    variable modified

--- src/restaurant/Settings.java
+++ src/restaurant/Settings.java
@@ -4,7 +4,7 @@ public class Settings {
 
     public static double endSimulationTime =600;
 
-    //public static final double CAR_WAH_PROBABILITY =0.1 ;
+    public static final double CUSTOMER_IMPATIENT_PROBABILITY =0.1 ;
     public static final int MAX_CUSTOMERS_PER_CYCLE = 4;
 
 }
