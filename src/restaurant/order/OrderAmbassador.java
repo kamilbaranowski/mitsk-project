ce358a34fb497e5af4c88b1d1d44e12ef1057ba2
KamilBaranowski <tmp2206@gmail.com>
2019-11-25 20:20:51 +0100

    order interactions

--- src/restaurant/order/OrderAmbassador.java
+++ src/restaurant/order/OrderAmbassador.java
@@ -1,11 +1,17 @@
 package restaurant.order;
 
+import hla.rti1516e.InteractionClassHandle;
 import restaurant.Ambassador;
 import restaurant.Federate;
 
 public class OrderAmbassador extends Ambassador {
 
     private OrderFederate orderFederate;
+
+    protected InteractionClassHandle orderExecutionHandle;
+    protected InteractionClassHandle placeOrderHandle;
+
+
 
     public OrderAmbassador(OrderFederate orderFederate) {
         this.orderFederate = orderFederate;
