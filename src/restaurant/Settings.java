7805af424b7509400845ebdae7643cc14bf99e50
KamilBaranowski <tmp2206@gmail.com>
2019-12-07 16:45:40 +0100

    max table variable

--- src/restaurant/Settings.java
+++ src/restaurant/Settings.java
@@ -3,7 +3,7 @@ package restaurant;
 public class Settings {
 
     public static double endSimulationTime =600;
-
+    public static final int MAX_TABLE = 50;
     public static final double CUSTOMER_IMPATIENT_PROBABILITY =0.1 ;
     public static final int MAX_CUSTOMERS_PER_CYCLE = 4;
 
