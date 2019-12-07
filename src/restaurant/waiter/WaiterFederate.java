764ddadf2782c35682c263c65726fd4a08268641
KamilBaranowski <tmp2206@gmail.com>
2019-12-07 20:13:22 +0100

    WaiterFederate: send payment interaction

--- src/restaurant/waiter/WaiterFederate.java
+++ src/restaurant/waiter/WaiterFederate.java
@@ -3,6 +3,7 @@ package restaurant.waiter;
 import hla.rti1516e.InteractionClassHandle;
 import hla.rti1516e.ParameterHandle;
 import hla.rti1516e.ParameterHandleValueMap;
+import hla.rti1516e.encoding.HLAASCIIstring;
 import hla.rti1516e.encoding.HLAinteger32BE;
 import hla.rti1516e.exceptions.*;
 import hla.rti1516e.time.HLAfloat64Time;
@@ -45,6 +46,27 @@ public class WaiterFederate extends Federate {
         rtiamb.subscribeInteractionClass(placeOrderInteractionHandle);
         rtiamb.subscribeInteractionClass(orderExecutionInteractionHandle);
 
+    }
+
+    public void sendPaymentInteraction(int amount, String type) throws NotConnected, FederateNotExecutionMember, NameNotFound, RTIinternalError, InvalidInteractionClassHandle, SaveInProgress, RestoreInProgress, InteractionClassNotPublished, InteractionClassNotDefined, InvalidLogicalTime, InteractionParameterNotDefined {
+        ParameterHandleValueMap parameters = rtiamb.getParameterHandleValueMapFactory().create(0);
+
+        HLAinteger32BE amountValue = encoderFactory.createHLAinteger32BE(amount);
+        HLAASCIIstring typeValue = encoderFactory.createHLAASCIIstring(type);
+
+
+        InteractionClassHandle interactionHandle = rtiamb.getInteractionClassHandle("HLAinteractionRoot.payment");
+
+        ParameterHandle amountHandle = rtiamb.getParameterHandle( interactionHandle,"amount" );
+        ParameterHandle typeHandle = rtiamb.getParameterHandle( interactionHandle,"type" );
+
+        parameters.put(amountHandle, amountValue.toByteArray());
+        parameters.put(typeHandle, typeValue.toByteArray());
+
+
+        log("Sending, payment, amount: " + amount + " type: " + type);
+        HLAfloat64Time time = timeFactory.makeTime( fedamb.federateTime+fedamb.federateLookahead );
+        rtiamb.sendInteraction( interactionHandle, parameters, generateTag(), time );
     }
 
     public void sendEndServiceInteraction(int tableNumber) throws NotConnected, FederateNotExecutionMember, NameNotFound, RTIinternalError, InvalidInteractionClassHandle, SaveInProgress, RestoreInProgress, InteractionClassNotPublished, InteractionClassNotDefined, InvalidLogicalTime, InteractionParameterNotDefined {
